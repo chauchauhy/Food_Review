5b4c82933687393e2808d3a8a8631abd409f7d53
chauchauhy <50822564+chauchauhy@users.noreply.github.com>
2019-08-06 16:25:51 +0800

    Notification

--- app/src/main/java/com/example/initapp/MainActivity.java
+++ app/src/main/java/com/example/initapp/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.initapp;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
 public class MainActivity extends AppCompatActivity {
+    EditText username, useraccount , userpassword, useremail;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initui();
     }
+    private void initui(){
+        username = findViewById(R.id.Username);
+        useraccount = findViewById(R.id.Useraccount);
+        userpassword = findViewById(R.id.Userpassword);
+        useremail = findViewById(R.id.Useremail);
+    }
+
+    private void noitication(){
+        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+            @Override
+            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                    if(! task.isSuccessful())
+                        return;
+                    if(task.getResult()==null)
+                        return;
+
+
+                    String token =task.getResult().getToken();
+                Log.i("TokenMain:", token);
+
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    String channelId = "default_notification_channel_id";
+                    String channelName = "default_notification_channel_name";
+                    NotificationManager notificationManager =
+                            getSystemService(NotificationManager.class);
+                    notificationManager.createNotificationChannel(new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_LOW));
+                }
+            }
+        });
+    }
+
 }
---
+++ app/src/main/java/com/example/initapp/firebaseSDK/Firebase_Cloud_Message.java
@@ -0,0 +1,68 @@
+package com.example.initapp.firebaseSDK;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import com.example.initapp.MainActivity;
+import com.example.initapp.R;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class Firebase_Cloud_Message extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        if (remoteMessage.getNotification() != null) {
+            Log.i("message title", "MessageFirebase ;" + remoteMessage.getNotification().getTitle());
+            Log.i("message body", "MessageFirebase ;" + remoteMessage.getNotification().getBody());
+            sendNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+
+        }
+
+    }
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        if (s != null)
+            Log.i("NewToken ", "Token    ;" + s);
+    }
+
+        private void sendNotification (String messageTitle , String messageBody){
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 , intent,PendingIntent.FLAG_ONE_SHOT);
+
+            String channelId = "default_notification_channel_id";
+            Uri defaultSoundUri =
+                    RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this,channelId)
+                    .setContentTitle(messageTitle)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentText(messageBody)
+                    .setAutoCancel(true)
+                    .setSound(defaultSoundUri)
+                    .setContentIntent(pendingIntent);
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
+                NotificationChannel channel = new NotificationChannel(channelId,"Channel Human Readable Title", NotificationManager.IMPORTANCE_DEFAULT);
+                notificationManager.createNotificationChannel(channel);
+            }
+            notificationManager.notify(0, notificationBuilder.build());
+
+
+        }
+    }
