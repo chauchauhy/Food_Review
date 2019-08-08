15fcc5045c67f69a775523d5a17e53877cc32450
chauchauhy <50822564+chauchauhy@users.noreply.github.com>
2019-08-08 10:24:32 +0800

    login stc

--- app/src/main/java/com/example/initapp/MainActivity.java
+++ app/src/main/java/com/example/initapp/MainActivity.java
@@ -5,31 +5,45 @@ import android.app.NotificationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {          //login page
     EditText username, useraccount , userpassword, useremail;
-
-
+    Button send;
+    LinearLayout linearLayout;
+    static String st_str_account;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initui();
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chknull();
+            }
+        });
+
     }
     private void initui(){
         username = findViewById(R.id.Username);
         useraccount = findViewById(R.id.Useraccount);
         userpassword = findViewById(R.id.Userpassword);
         useremail = findViewById(R.id.Useremail);
+        send = findViewById(R.id.send);
+        linearLayout = findViewById(R.id.login_Linear);
     }
 
     private void noitication(){
@@ -40,8 +54,6 @@ public class MainActivity extends AppCompatActivity {
                         return;
                     if(task.getResult()==null)
                         return;
-
-
                     String token =task.getResult().getToken();
                 Log.i("TokenMain:", token);
 
@@ -56,5 +68,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void chknull(){
+        initui();
+        Boolean boolean_result = false;
+        String str_username = "username";
+        String str_useraccount = "account";
+        String str_userpassword = "password";
+        String str_useremail = "Email";
+        String warring = " is invaild";
+        if(username.getText().toString().trim().equals("")) {
+            Snackbar.make(linearLayout, str_username + warring, Snackbar.LENGTH_SHORT).show();
+        if(useraccount.getText().toString().trim().length()<5){
+            Snackbar.make(linearLayout, str_useraccount + warring, Snackbar.LENGTH_SHORT).show();
+        if (userpassword.getText().toString().trim().length()<5){
+            Snackbar.make(linearLayout, str_userpassword+ warring, Snackbar.LENGTH_SHORT).show();
+        if(!(useremail.getText().toString().trim().length()<7 && useremail.getText().toString().trim().contains("@")&&useremail.getText().toString().trim().contains(".com"))) {
+            Snackbar.make(linearLayout, str_useremail + warring, Snackbar.LENGTH_SHORT).show();
+        }}}}else {
+            Snackbar.make(linearLayout,"right!", Snackbar.LENGTH_SHORT).show();
+        }
+//        return false;
+    }
+
 
 }
---
+++ app/src/main/java/com/example/initapp/ShowRes.java
@@ -0,0 +1,16 @@
+package com.example.initapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowRes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_res);
+
+
+    }
+}
