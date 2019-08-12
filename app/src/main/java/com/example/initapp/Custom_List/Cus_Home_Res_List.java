12939a397a14bf22fb6e4b1323757f4028737dcf
chauchauhy <50822564+chauchauhy@users.noreply.github.com>
2019-08-12 11:18:07 +0800

    Cus

--- app/src/main/java/com/example/initapp/Custom_List/Cus_Home_Res_List.java
+++ app/src/main/java/com/example/initapp/Custom_List/Cus_Home_Res_List.java
@@ -1,17 +1,22 @@
 package com.example.initapp.Custom_List;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.initapp.Home;
+import com.example.initapp.MainActivity;
 import com.example.initapp.R;
+import com.example.initapp.login;
 import com.example.initapp.model.Restaurant;
 import com.squareup.picasso.Picasso;
 
@@ -23,10 +28,22 @@ import static com.example.initapp.SetUp.url_all_image;
 public class Cus_Home_Res_List extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     LayoutInflater layoutInflater;
     ArrayList<Restaurant> restaurants=new ArrayList<Restaurant>();
+    Context context;
+    Restaurant restaurant;
+    private OnItemClickListener listener;
+
+    public interface OnItemClickListener{
+        void onItemClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        this.listener = listener;
+    }
 
     public Cus_Home_Res_List(Context context, ArrayList<Restaurant> restaurants) {
         this.layoutInflater = LayoutInflater.from(context);
         this.restaurants = restaurants;
+        this.context = context;
     }
 
 
@@ -35,22 +52,32 @@ public class Cus_Home_Res_List extends RecyclerView.Adapter<RecyclerView.ViewHol
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
         View item = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cus_row_home_res,viewGroup,false);
         RecyclerView.ViewHolder holder = new Res_Home_List(item);
+
+
         return holder;
     }
+//    private void putdata(String id){
+//        Intent i = new Intent(context, login.class);
+//        i.putExtra("restaurantID",id);
+//        context.startActivity(i);
+//
+//    }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int i) {
         Res_Home_List home_list = (Res_Home_List) holder;
-        Restaurant restaurant = restaurants.get(i);
+        restaurant = restaurants.get(i);
         String urll = url_all_image+restaurant.getResImage();
         Picasso.with(layoutInflater.getContext())
                 .load(urll)
                 .placeholder(R.drawable.btn_back)
+                .fit()
                 .error(R.drawable.btn_add)
                 .into( home_list.res_image);
         Log.i("httpul",url_all_image+restaurant.getResImage());
         home_list.res_name.setText(restaurant.getResName());
         home_list.res_detail.setText(restaurant.getResDetail());
+
 
     }
 
@@ -68,15 +95,28 @@ public class Cus_Home_Res_List extends RecyclerView.Adapter<RecyclerView.ViewHol
     public class Res_Home_List extends  RecyclerView.ViewHolder {
         ImageView res_image;
         TextView res_name , res_detail;
+        LinearLayout cus_linear;
 
         public Res_Home_List(@NonNull View itemView) {
             super(itemView);
             res_image = itemView.findViewById(R.id.firs_res_imaget);
             res_name = itemView.findViewById(R.id.res_Name_home);
             res_detail = itemView.findViewById(R.id.res_detail);
+            cus_linear = itemView.findViewById(R.id.cus_linear);
 
-
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(listener!=null){
+                        int position = getAdapterPosition();
+                        if(position!= RecyclerView.NO_POSITION){
+                            listener.onItemClick(position);
+                        }
+                    }
+                }
+            });
         }
+
     }
 
 
--- app/src/main/java/com/example/initapp/login.java
+++ app/src/main/java/com/example/initapp/login.java
@@ -2,7 +2,11 @@ package com.example.initapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import static com.example.initapp.Home.resID;
 
 public class login extends AppCompatActivity {
 
@@ -10,5 +14,8 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Intent i = getIntent();
+        String id = i.getStringExtra(resID);
+        Log.i("Stringid","XXXXXXXXXXXXXXX"+ id);
     }
 }
